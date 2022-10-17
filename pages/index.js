b0574ba98831e7e30c29335a60b0d750644ce37f
Greg Vermaak <gregory.vermaak@developersinstitute.co.nz>
2022-10-18 11:08:27 +1300

    updated skills section

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,27 @@ import Head from "next/head";
 import { useState } from "react";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import {
+	SiReact,
+	SiHtml5,
+	SiCss3,
+	SiTailwindcss,
+	SiJavascript,
+	SiNextdotjs,
+	SiNodedotjs,
+	SiExpress,
+	SiFirebase,
+	SiMongodb,
+	SiDocker,
+	SiPostgresql,
+	SiGit,
+	SiGithub,
+} from "react-icons/si";
 import Image from "next/image";
 import memoji from "../public/memoji.png";
-import design from "../public/design.png";
+import computer from "../public/computer.png";
 import code from "../public/code.png";
-import consulting from "../public/consulting.png";
+import database from "../public/database.png";
 import web1 from "../public/web1.png";
 import web2 from "../public/web2.png";
 import web3 from "../public/web3.png";
@@ -53,10 +69,10 @@ export default function Home() {
 							Greg Vermaak
 						</h2>
 						<h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-							Front End Developer.
+							Full Stack Developer.
 						</h3>
 						<p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-							Front End Dev driven by passion. Hailing all the way from New
+							Full Stack dev driven by passion. Hailing all the way from New
 							Zealand!
 						</p>
 					</div>
@@ -88,85 +104,95 @@ export default function Home() {
 				{/* Experience Section */}
 				<section>
 					<div>
-						<h3 className="text-3xl py-1 dark:text-white">Services I offer</h3>
+						<h3 className="text-3xl py-1 dark:text-white">About Me</h3>
 						<p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-							Since the beginning of my journey as a freelance designer and
-							developer, I&apos;ve done remote work for
-							<span className="text-teal-500"> agencies </span>
-							consulted for <span className="text-teal-500">startups </span>
-							and collaborated with talanted people to create digital products
-							for both business and consumer use.
+							My name is Greg, I am a Full Stack dev from New Zealand. I create
+							bespoke websites to help people go further online.
 						</p>
 						<p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-							I offer from a wide range of services, including brand design,
-							programming and teaching.
+							Since the beginning of my journey as a developer, I&apos;ve done
+							remote work for
+							<span className="text-teal-500"> record labels </span>
+							and <span className="text-teal-500">startups </span>
+							and collaborated with talanted people to create digital products
+							for both business and consumer use.
 						</p>
 					</div>
 					<div className="lg:flex gap-10">
 						<div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
-							<Image alt="portfolio" src={design} width={100} height={100} />
-							<h3 className="text-lg font-medium pt-8 pb-2  ">
-								Beautiful Designs
-							</h3>
-							<p className="py-2">
-								Creating elegant designs suited for your needs following core
-								design theory.
-							</p>
-							<h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-							<p className="text-gray-800 py-1">Photoshop</p>
-							<p className="text-gray-800 py-1">Illustrator</p>
-							<p className="text-gray-800 py-1">Figma</p>
-							<p className="text-gray-800 py-1">Indesign</p>
+							<Image alt="portfolio" src={computer} width={100} height={100} />
+							<h3 className="text-lg font-medium pt-8 pb-2">Client Side</h3>
+							<h4 className="py-4 text-teal-600">Tools I Use</h4>
+							<div className="flex items-center justify-around text-5xl mt-6">
+								<p className="text-gray-800 py-1">
+									<SiHtml5 />
+								</p>
+								<p className="text-gray-800 py-1">
+									<SiCss3 />
+								</p>
+								<p className="text-gray-800 py-1">
+									<SiTailwindcss />
+								</p>
+								<p className="text-gray-800 py-1">
+									<SiJavascript />
+								</p>
+								<p className="text-gray-800 py-1">
+									<SiReact />
+								</p>
+								<p className="text-gray-800 py-1">
+									<SiNextdotjs />
+								</p>
+							</div>
 						</div>
 						<div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
 							<Image alt="portfolio" src={code} width={100} height={100} />
-							<h3 className="text-lg font-medium pt-8 pb-2">
-								Code your dream project
-							</h3>
-							<p className="py-2">
-								Do you have an idea for your next great website? Let&apos;s make
-								it a reality.
-							</p>
-							<h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-							<p className="text-gray-800 py-1">Photoshop</p>
-							<p className="text-gray-800 py-1">Illustrator</p>
-							<p className="text-gray-800 py-1">Figma</p>
-							<p className="text-gray-800 py-1">Indesign</p>
+							<h3 className="text-lg font-medium pt-8 pb-2">Server Side</h3>
+							<h4 className="py-4 text-teal-600">Tools I Use</h4>
+							<div className="flex items-center justify-around text-5xl mt-6">
+								<p className="text-gray-800 py-1">
+									<SiNodedotjs />
+								</p>
+								<p className="text-gray-800 py-1">
+									<SiExpress />
+								</p>
+								<p className="text-gray-800 py-1">
+									<SiFirebase />
+								</p>
+							</div>
 						</div>
 						<div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-							<Image
-								alt="portfolio"
-								src={consulting}
-								width={100}
-								height={100}
-							/>
-							<h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
-							<p className="py-2">
-								Are you interested in feedback for your current project? I can
-								give you tips and tricks to level it up.
-							</p>
-							<h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-							<p className="text-gray-800 py-1">Photoshop</p>
-							<p className="text-gray-800 py-1">Illustrator</p>
-							<p className="text-gray-800 py-1">Figma</p>
-							<p className="text-gray-800 py-1">Indesign</p>
+							<Image alt="portfolio" src={database} width={100} height={100} />
+							<h3 className="text-lg font-medium pt-8 pb-2 ">
+								Databases & Miscellaneous
+							</h3>
+							<h4 className="py-4 text-teal-600">Tools I Use</h4>
+							<div className="flex items-center justify-around text-5xl mt-6">
+								<p className="text-gray-800 py-1">
+									<SiMongodb />
+								</p>
+								<p className="text-gray-800 py-1">
+									<SiPostgresql />
+								</p>
+								<p className="text-gray-800 py-1">
+									<SiFirebase />
+								</p>
+								<p className="text-gray-800 py-1">
+									<SiDocker />
+								</p>
+								<p className="text-gray-800 py-1">
+									<SiGit />
+								</p>
+							</div>
 						</div>
 					</div>
 				</section>
 				<section className="py-10">
 					<div>
-						<h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
+						<h3 className="text-3xl py-1 dark:text-white ">Portfolio</h3>
+
 						<p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-							Since the beginning of my journey as a freelance designer and
-							developer, I&apos;ve done remote work for
-							<span className="text-teal-500"> agencies </span>
-							consulted for <span className="text-teal-500">startups </span>
-							and collaborated with talanted people to create digital products
-							for both business and consumer use.
-						</p>
-						<p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-							I offer from a wide range of services, including brand design,
-							programming and teaching.
+							Below are some of the pieces that I am most proud of. Ranging from
+							personal projects to client work.
 						</p>
 					</div>
 					<div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
